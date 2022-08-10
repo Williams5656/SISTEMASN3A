fac8b6eac257c8dde59664edbc9bd7263e7121cb
damia <damia@WilmerPC>
2022-08-09 20:08:46 -0500

    class

--- src/Hertz/main.java
+++ src/Hertz/main.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package HERTZ_ew;
+package Hertz;
 
 /**
  *
  * @author damia
  */
-public class classWilmer {
+public class main {
     
 }
