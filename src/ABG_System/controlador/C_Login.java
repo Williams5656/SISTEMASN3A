13d1287c503ecbdb102f421445fcc9ef8611862e
LENOVO <LENOVO@LAPTOP-PC2GTBE7>
2022-08-10 17:05:40 -0500

    Practica_Oscar

--- src/ABG_System/controlador/C_Login.java
+++ src/ABG_System/controlador/C_Login.java
@@ -7,18 +7,28 @@ import java.awt.Font;
 public class C_Login {
 
     public static V_Login vista;
+    int xMouse, yMouse;
+    int bloqueo = 3;
+    boolean mostrar;
+    String tUsuario = "USUARIO:";
+    String pClave = "CONTRASEÑA:";
 
     public C_Login(V_Login vista) {
         this.vista = vista;
         vista.setVisible(true);
         vista.setLocationRelativeTo(null);
-        vista.getJpassUser().setVisible(false);
+        vista.getTxtPassUser().setVisible(false);
+        vista.getBtn_Cerrar().setVisible(false);
+        vista.getLabelMostrar().setVisible(false);
+        vista.getTxtUsuario().setText(tUsuario);
+        vista.getTxtUsuario().setFont(new Font("Miriam Fixed", 3, 14));
+        vista.getJpassUser().setText(pClave);
+        vista.getJpassUser().setFont(new Font("Miriam Fixed", 3, 14));
         vista.getTxtUsuario().addFocusListener(new java.awt.event.FocusAdapter() {
             @Override
             public void focusGained(java.awt.event.FocusEvent evt) {
                 txtUsuarioFocusGained(evt);
             }
-
             @Override
             public void focusLost(java.awt.event.FocusEvent evt) {
                 txtUsuarioFocusLost(evt);
@@ -29,62 +39,66 @@ public class C_Login {
             public void focusGained(java.awt.event.FocusEvent evt) {
                 JpassUserFocusGained(evt);
             }
-
             @Override
             public void focusLost(java.awt.event.FocusEvent evt) {
                 JpassUserFocusLost(evt);
             }
         });
+        vista.getLabelOcultar().addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                labelOcultarMouseEntered(evt);
+            }
+        });
     }
 
     private void txtUsuarioFocusGained(java.awt.event.FocusEvent evt) {
-        vista.getTxtUsuario().setForeground(Color.BLACK);
-        if (vista.getTxtUsuario().getText().isEmpty()) {
-            vista.getLabelUsuario().setText("USUARIO: ");
-            vista.getTxtUsuario().setText("");
-        } else {
-            vista.getTxtUsuario().setText(vista.getTxtUsuario().getText());
-            vista.getTxtUsuario().setForeground(Color.BLACK);
-            vista.getTxtUsuario().setFont(new Font("Arial", 3, 14));
-
-        }
-
+        vista.getTxtUsuario().setText("");
+        vista.getLabelUsuario().setText(tUsuario);
+        vista.getTxtUsuario().setEditable(true);
+        vista.getTxtUsuario().setFont(new Font("Arial", 3, 14));
     }
 
     private void txtUsuarioFocusLost(java.awt.event.FocusEvent evt) {
-        vista.getTxtUsuario().setText("USUARIO: ");
-        vista.getLabelUsuario().setText("");
-        vista.getTxtUsuario().setForeground(Color.GRAY);
-        vista.getTxtUsuario().setFont(new Font("Serif", 3, 24));
-    }
-
-    private void txtPassUserFocusGained(java.awt.event.FocusEvent evt) {
-        vista.getTxtPassUser().setVisible(false);
-        vista.getJpassUser().setVisible(true);
-        if (vista.getJpassUser().getText().isEmpty()) {
-            vista.getJpassUser().setText(vista.getJpassUser().getText());
-        } else {
-            vista.getLabelclave().setText("CONTRASEÑA: ");
-            vista.getJpassUser().setText("");
+        if (vista.getTxtUsuario().getText().isEmpty()) {
+            vista.getTxtUsuario().setText(tUsuario);
+            vista.getLabelUsuario().setText("");
+            vista.getTxtUsuario().setEditable(false);
         }
 
     }
 
-    private void JpassUserFocusLost(java.awt.event.FocusEvent evt) {
-        vista.getJpassUser().setVisible(false);
-        vista.getTxtPassUser().setVisible(true);
-        vista.getTxtPassUser().setForeground(Color.GRAY);
-        vista.getTxtPassUser().setFont(new Font("Serif", 3, 24));
-        vista.getLabelclave().setText("");
-        vista.getTxtPassUser().setText("CONTRASEÑA: ");
-    }
-
     private void JpassUserFocusGained(java.awt.event.FocusEvent evt) {
-        vista.getLabelclave().setText("CONTRASEÑA: ");
         vista.getJpassUser().setText("");
+        vista.getLabelclave().setText(pClave);
+        vista.getJpassUser().setEditable(true);
+        vista.getJpassUser().setFont(new Font("Arial", 3, 14));
     }
 
-    private void txtPassUserFocusLost(java.awt.event.FocusEvent evt) {
+    private void JpassUserFocusLost(java.awt.event.FocusEvent evt) {
+        if (vista.getJpassUser().getText().isEmpty()) {
+            vista.getJpassUser().setText(pClave);
+            vista.getLabelclave().setText("");
+            vista.getJpassUser().setEditable(false);
+        }
 
+    }
+     private void labelOcultarMouseEntered(java.awt.event.MouseEvent evt) {                                          
+       if (mostrar) {
+            vista.getLabelMostrar().setVisible(true);
+            vista.getLabelOcultar().setVisible(false);
+            vista.getTxtPassUser().setVisible(false);
+            vista.getJpassUser().setVisible(true);
+            this.vista.getJpassUser().setForeground(Color.BLACK);
+            vista.getJpassUser().setText(vista.getTxtPassUser().getText());
+            mostrar = false;
+        } else {
+            vista.getLabelMostrar().setVisible(false);
+            vista.getLabelOcultar().setVisible(true);
+            vista.getTxtPassUser().setVisible(true);
+            vista.getJpassUser().setVisible(false);
+            vista.getTxtPassUser().setText(vista.getJpassUser().getText());
+            mostrar = true;
+        }
     }
 }
--- src/ABG_System/vista/V_Login.java
+++ src/ABG_System/vista/V_Login.java
@@ -61,17 +61,65 @@ public class V_Login extends javax.swing.JFrame {
         return txtUsuario;
     }
 
+    public JPanel getPanelCerrar() {
+        return PanelCerrar;
+    }
+
+    public void setPanelCerrar(JPanel PanelCerrar) {
+        this.PanelCerrar = PanelCerrar;
+    }
+
     public void setTxtUsuario(JTextField txtUsuario) {
         this.txtUsuario = txtUsuario;
+    }
+
+    public JLabel getAnimacion_Cerrar() {
+        return Animacion_Cerrar;
+    }
+
+    public void setAnimacion_Cerrar(JLabel Animacion_Cerrar) {
+        this.Animacion_Cerrar = Animacion_Cerrar;
+    }
+
+    public JPanel getBarraMovi() {
+        return BarraMovi;
+    }
+
+    public void setBarraMovi(JPanel BarraMovi) {
+        this.BarraMovi = BarraMovi;
+    }
+
+    public JLabel getBtn_Cerrar() {
+        return Btn_Cerrar;
+    }
+
+    public void setBtn_Cerrar(JLabel Btn_Cerrar) {
+        this.Btn_Cerrar = Btn_Cerrar;
+    }
+
+    public JLabel getLabelMostrar() {
+        return labelMostrar;
+    }
+
+    public void setLabelMostrar(JLabel labelMostrar) {
+        this.labelMostrar = labelMostrar;
+    }
+
+    public JLabel getLabelOcultar() {
+        return labelOcultar;
+    }
+
+    public void setLabelOcultar(JLabel labelOcultar) {
+        this.labelOcultar = labelOcultar;
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        ImageIcon icon = new ImageIcon(getClass().getResource("/images/Login.png"));
+        ImageIcon icon = new ImageIcon(getClass().getResource("/ABG_System/controlador/Login.png"));
         Image image = icon.getImage();
-        jPanel1 = new javax.swing.JPanel(){
+        Fondo = new javax.swing.JPanel(){
             public void paintComponent(Graphics g){
                 g.drawImage(image,0,0,getWidth(),getHeight(),this);
             }
@@ -84,66 +132,107 @@ public class V_Login extends javax.swing.JFrame {
         JpassUser = new javax.swing.JPasswordField();
         txtPassUser = new javax.swing.JTextField();
         BtnIngresar = new javax.swing.JButton();
+        BarraMovi = new javax.swing.JPanel();
+        PanelCerrar = new javax.swing.JPanel();
+        Animacion_Cerrar = new javax.swing.JLabel();
+        Btn_Cerrar = new javax.swing.JLabel();
+        labelMostrar = new javax.swing.JLabel();
+        labelOcultar = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setLocationByPlatform(true);
+        setUndecorated(true);
+        setResizable(false);
 
-        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        Fondo.setBackground(new java.awt.Color(3, 3, 3));
+        Fondo.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         labelUsuario.setFont(new java.awt.Font("Script MT Bold", 3, 24)); // NOI18N
         labelUsuario.setForeground(new java.awt.Color(164, 224, 240));
-        jPanel1.add(labelUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 110, 170, 60));
+        Fondo.add(labelUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 110, 170, 60));
 
         labelclave.setFont(new java.awt.Font("Script MT Bold", 3, 24)); // NOI18N
         labelclave.setForeground(new java.awt.Color(164, 224, 240));
-        jPanel1.add(labelclave, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 240, 280, 60));
+        Fondo.add(labelclave, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 240, 280, 60));
 
         LabelTitulo.setFont(new java.awt.Font("Script MT Bold", 3, 36)); // NOI18N
         LabelTitulo.setForeground(new java.awt.Color(164, 224, 240));
         LabelTitulo.setText("    ABG ");
-        jPanel1.add(LabelTitulo, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 40, 170, 60));
+        Fondo.add(LabelTitulo, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 50, 170, 60));
 
         txtUsuario.setFont(new java.awt.Font("Serif", 3, 24)); // NOI18N
         txtUsuario.setForeground(new java.awt.Color(153, 153, 153));
-        txtUsuario.setText("Usuario:");
-        jPanel1.add(txtUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 180, 250, 60));
-
-        JpassUser.setText("jPasswordField1");
-        jPanel1.add(JpassUser, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 310, 260, 60));
+        Fondo.add(txtUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 180, 250, 60));
+        Fondo.add(JpassUser, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 310, 260, 60));
 
         txtPassUser.setFont(new java.awt.Font("Serif", 3, 24)); // NOI18N
         txtPassUser.setForeground(new java.awt.Color(153, 153, 153));
         txtPassUser.setText("Contraseña:");
-        jPanel1.add(txtPassUser, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 310, 260, 60));
+        Fondo.add(txtPassUser, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 310, 260, 60));
 
         BtnIngresar.setBackground(new java.awt.Color(186, 252, 245));
         BtnIngresar.setFont(new java.awt.Font("Yu Gothic UI Semibold", 2, 18)); // NOI18N
         BtnIngresar.setText("INGRESAR");
         BtnIngresar.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        jPanel1.add(BtnIngresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 400, 160, 50));
+        Fondo.add(BtnIngresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 400, 160, 50));
+
+        BarraMovi.setBackground(new java.awt.Color(3, 3, 3));
+        BarraMovi.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        PanelCerrar.setBackground(new java.awt.Color(3, 3, 3));
+        PanelCerrar.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        Animacion_Cerrar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ABG_System/controlador/cerca.png"))); // NOI18N
+        PanelCerrar.add(Animacion_Cerrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 46, 60));
+
+        Btn_Cerrar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ABG_System/controlador/cerrar-sesion.png"))); // NOI18N
+        PanelCerrar.add(Btn_Cerrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(-3, 0, 50, 60));
+
+        BarraMovi.add(PanelCerrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 0, -1, -1));
+
+        Fondo.add(BarraMovi, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 480, 60));
+
+        labelMostrar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ABG_System/controlador/ojo.png"))); // NOI18N
+        Fondo.add(labelMostrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 300, 80, 70));
+
+        labelOcultar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ABG_System/controlador/esconder.png"))); // NOI18N
+        labelOcultar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                labelOcultarMouseEntered(evt);
+            }
+        });
+        Fondo.add(labelOcultar, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 300, 70, 70));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 490, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(Fondo, javax.swing.GroupLayout.PREFERRED_SIZE, 476, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 510, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 6, Short.MAX_VALUE))
+            .addComponent(Fondo, javax.swing.GroupLayout.PREFERRED_SIZE, 504, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void labelOcultarMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_labelOcultarMouseEntered
+      
+    }//GEN-LAST:event_labelOcultarMouseEntered
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel Animacion_Cerrar;
+    private javax.swing.JPanel BarraMovi;
     private javax.swing.JButton BtnIngresar;
+    private javax.swing.JLabel Btn_Cerrar;
+    private javax.swing.JPanel Fondo;
     private javax.swing.JPasswordField JpassUser;
     private javax.swing.JLabel LabelTitulo;
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel PanelCerrar;
+    private javax.swing.JLabel labelMostrar;
+    private javax.swing.JLabel labelOcultar;
     private javax.swing.JLabel labelUsuario;
     private javax.swing.JLabel labelclave;
     private javax.swing.JTextField txtPassUser;
