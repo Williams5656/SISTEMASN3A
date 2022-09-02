051417cfbefe51c7f9a6af5657f3168a760cc4f7
VICO5 <VICO5@DESKTOP-BAU5561>
2022-09-02 16:46:27 -0500

    hola

---
+++ src/Modelo/productoMD.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.awt.Image;
+
+/**
+ *
+ * @author VICO5
+ */
+public class productoMD {
+    String id_producto;
+    String nombre_pro;
+    String descripcion_pro;
+    Image foto;
+    
+}
